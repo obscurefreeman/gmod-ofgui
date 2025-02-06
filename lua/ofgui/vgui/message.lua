68b023811b01b2f53533e2274267de18bbf884e8
晦涩弗里曼 <1093048022@qq.com>
2025-02-06 21:45:30 +0800

    修改对话菜单

--- lua/ofgui/vgui/message.lua
+++ lua/ofgui/vgui/message.lua
@@ -5,40 +5,45 @@ function PANEL:Init()
 
     self.Color = Color(OFGUI.ButtonColor.r, OFGUI.ButtonColor.g, OFGUI.ButtonColor.b, OFGUI.ButtonColor.a)
     
-    -- 默认文本
-    self.Title = ""
-    self.Description = ""
+    -- 创建标题DLabel
+    self.TitleLabel = vgui.Create("DLabel", self)
+    self.TitleLabel:SetFont("ofgui_big")
+    self.TitleLabel:SetTextColor(OFGUI.ButtonTextColor)
+    self.TitleLabel:Dock(TOP)
+    
+    -- 创建描述DLabel
+    self.DescLabel = vgui.Create("DLabel", self)
+    self.DescLabel:SetFont("ofgui_medium")
+    self.DescLabel:SetTextColor(OFGUI.ButtonTextColor)
+    self.DescLabel:SetWrap(true)
+    self.DescLabel:Dock(TOP)
     
     -- 默认大小
     self:SetSize(300, 64)
 end
 
 function PANEL:SetName(text)
-    self.Title = text
+    self.TitleLabel:SetText(text)
+    self.TitleLabel:SetAutoStretchVertical(true)
+    self:UpdateLayout()
 end
 
 function PANEL:SetText(text)
-    self.Description = text
+    self.DescLabel:SetText(text)
+    self.DescLabel:SetAutoStretchVertical(true)
+    self:UpdateLayout()
+end
+
+function PANEL:UpdateLayout()
+    -- 更新面板高度
+    local padding = 4 * OFGUI.ScreenScale
+    local totalHeight = self.TitleLabel:GetTall() + padding + self.DescLabel:GetTall() + padding
+    self:SetTall(totalHeight)
 end
 
 function PANEL:Paint(w, h)
-    -- 绘制背景
+    -- 只需绘制背景
     draw.RoundedBox(0, 0, 0, w, h, self.Color)
-    
-    -- 计算文本高度
-    local titleHeight = draw.GetFontHeight("ofgui_big")
-    local descHeight = draw.GetFontHeight("ofgui_medium")
-    local totalTextHeight = titleHeight + descHeight + 5 * OFGUI.ScreenScale  -- 5是标题和描述之间的间距
-    
-    -- 计算整体文本块的起始Y坐标，使其垂直居中
-    local startY = (h - totalTextHeight) / 2
-    
-    -- 文本X坐标调整
-    local titleX = 8 * OFGUI.ScreenScale
-    
-    -- 绘制标题和描述
-    draw.SimpleText(self.Title, "ofgui_big", titleX, startY, OFGUI.ButtonTextColor)
-    draw.SimpleText(self.Description, "ofgui_medium", titleX, startY + titleHeight + 5, OFGUI.ButtonTextColor)
 end
 
 derma.DefineControl("OFMessage", "NPC信息区域", PANEL, "DPanel") 

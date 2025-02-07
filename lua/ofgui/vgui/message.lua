2c64009c979e884d6ca73d403f30eed44935a299
晦涩弗里曼 <1093048022@qq.com>
2025-02-07 14:29:51 +0800

    改为标记语言

--- lua/ofgui/vgui/message.lua
+++ lua/ofgui/vgui/message.lua
@@ -5,44 +5,55 @@ function PANEL:Init()
 
     self.Color = Color(OFGUI.ButtonColor.r, OFGUI.ButtonColor.g, OFGUI.ButtonColor.b, OFGUI.ButtonColor.a)
     
-    -- 创建标题DLabel
-    self.TitleLabel = vgui.Create("DLabel", self)
-    self.TitleLabel:SetFont("ofgui_big")
-    self.TitleLabel:SetTextColor(Color(255, 255, 255, 255))
-    self.TitleLabel:DockMargin(4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 2 * OFGUI.ScreenScale)
-    self.TitleLabel:Dock(TOP)
-    
-    -- 创建描述DLabel
-    self.DescLabel = vgui.Create("DLabel", self)
-    self.DescLabel:SetFont("ofgui_medium")
-    self.DescLabel:SetTextColor(Color(255, 255, 255, 255))
-    self.DescLabel:SetWrap(true)
-    self.DescLabel:DockMargin(4 * OFGUI.ScreenScale, 2 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale, 4 * OFGUI.ScreenScale)
-    self.DescLabel:Dock(TOP)
-    
     -- 默认大小
     self:SetSize(300, 64)
+    
+    -- 初始化标题和描述文本
+    self.titleMarkup = nil
+    self.descMarkup = nil
+    self.titleText = ""    -- 保存原始标题文本
+    self.descText = ""     -- 保存原始描述文本
 end
 
 function PANEL:SetName(text)
-    self.TitleLabel:SetText(text)
-    self.TitleLabel:SetAutoStretchVertical(true)
+    -- 保存原始文本
+    self.titleText = text
+    -- 使用markup解析标题文本
+    self.titleMarkup = markup.Parse("<font=ofgui_big>" .. text .. "</font>")
 end
 
 function PANEL:SetText(text)
-    self.DescLabel:SetText(text)
-    self.DescLabel:SetAutoStretchVertical(true)
+    -- 保存原始文本
+    self.descText = text
+    -- 使用markup解析描述文本
+    self.descMarkup = markup.Parse("<font=ofgui_medium>" .. text .. "</font>", self:GetWide() - 8 * OFGUI.ScreenScale)
 end
 
 function PANEL:Think()
+    if not self.titleMarkup or not self.descMarkup then return end
+    
+    -- 使用保存的原始文本重新解析
+    self.descMarkup = markup.Parse("<font=ofgui_medium>" .. self.descText .. "</font>", self:GetWide() - 8 * OFGUI.ScreenScale)
+    
     local padding = 4 * OFGUI.ScreenScale
-    local totalHeight = self.TitleLabel:GetTall() + self.DescLabel:GetTall() + 3 * padding
+    local totalHeight = self.titleMarkup:GetHeight() + self.descMarkup:GetHeight() + 3 * padding
     self:SetTall(totalHeight)
 end
 
 function PANEL:Paint(w, h)
     -- 只需绘制背景
     draw.RoundedBox(0, 0, 0, w, h, self.Color)
+    
+    -- 绘制标题和描述文本
+    local padding = 4 * OFGUI.ScreenScale
+    
+    if self.titleMarkup then
+        self.titleMarkup:Draw(padding, padding, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    end
+    
+    if self.descMarkup then
+        self.descMarkup:Draw(padding, padding + (self.titleMarkup and self.titleMarkup:GetHeight() or 0) + padding, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    end
 end
 
 derma.DefineControl("OFMessage", "NPC信息区域", PANEL, "DPanel") 

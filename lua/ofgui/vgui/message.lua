43e02d157ea8de3685e5f82fd162acba8a2a66e9
晦涩弗里曼 <1093048022@qq.com>
2025-02-07 15:34:55 +0800

    修改消息样式

--- lua/ofgui/vgui/message.lua
+++ lua/ofgui/vgui/message.lua
@@ -3,7 +3,8 @@ local PANEL = {}
 function PANEL:Init()
     self:SetExpensiveShadow(1, ColorAlpha(color_black, 140))
 
-    self.Color = Color(OFGUI.ButtonColor.r, OFGUI.ButtonColor.g, OFGUI.ButtonColor.b, OFGUI.ButtonColor.a)
+    -- 修改为只存储条形的颜色
+    self.Color = Color(255, 255, 255)
     
     -- 默认大小
     self:SetSize(300, 64)
@@ -35,25 +36,36 @@ function PANEL:Think()
     -- 使用保存的原始文本重新解析
     self.descMarkup = markup.Parse("<font=ofgui_medium>" .. self.descText .. "</font>", self:GetWide() - 8 * OFGUI.ScreenScale)
     
-    local padding = 4 * OFGUI.ScreenScale
+    local padding = 6 * OFGUI.ScreenScale
     local totalHeight = self.titleMarkup:GetHeight() + self.descMarkup:GetHeight() + 3 * padding
     self:SetTall(totalHeight)
 end
 
 function PANEL:Paint(w, h)
-    -- 只需绘制背景
-    draw.RoundedBox(0, 0, 0, w, h, self.Color)
+    -- 绘制左侧条形
+    local barWidth = 5 * OFGUI.ScreenScale
+    draw.RoundedBox(0, 0, 0, barWidth, h, self.Color)
     
     -- 绘制标题和描述文本
-    local padding = 4 * OFGUI.ScreenScale
+    local padding = 6 * OFGUI.ScreenScale
+    local paddingleft = 16 * OFGUI.ScreenScale
     
     if self.titleMarkup then
-        self.titleMarkup:Draw(padding, padding, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+        -- 重新解析标题文本以使用条形颜色
+        self.titleMarkup = markup.Parse(string.format("<color=%d,%d,%d,255><font=ofgui_big>%s</font></color>",
+            self.Color.r, self.Color.g, self.Color.b, self.titleText))
+        self.titleMarkup:Draw(paddingleft + barWidth, padding, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
     end
     
     if self.descMarkup then
-        self.descMarkup:Draw(padding, padding + (self.titleMarkup and self.titleMarkup:GetHeight() or 0) + padding, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+        self.descMarkup:Draw(paddingleft + barWidth, padding + (self.titleMarkup and self.titleMarkup:GetHeight() or 0) + padding, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
     end
+end
+
+function PANEL:SetColor(col)
+    -- 确保输入是一个有效的Color对象
+    if not col or not col.r or not col.g or not col.b then return end
+    self.Color = Color(col.r, col.g, col.b)
 end
 
 derma.DefineControl("OFMessage", "NPC信息区域", PANEL, "DPanel") 
